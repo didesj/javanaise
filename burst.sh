53176f241ecc78810eda93a7b794d2c55164ae96
minchunnguyen <minchun_nguyen@yahoo.com>
2017-11-04 12:23:30 +0100

    modifier le shell et README

--- burst.sh
+++ burst.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+
+
+echo "le nombre de clients testés : "
+read nombre
 java -cp ./build/ jvn.JvnCoordImpl &
 
 sleep 10
 
-java -cp ./build/ test.SimulClient 1 &
+#java -cp ./build/ test.SimulClient 1 &
 
-sleep 1
+#sleep 1
 
-java -cp ./build/ test.SimulClient 2 &
+#java -cp ./build/ test.SimulClient 2 &
 
-sleep 1
+#sleep 1
 
-java -cp ./build/ test.SimulClient 3 &
+#java -cp ./build/ test.SimulClient 3 &
+
+
+for i in `seq 1 $nombre`
+do
+	java -cp ./build/ test.SimulClient $i &
+	sleep 1
+done
